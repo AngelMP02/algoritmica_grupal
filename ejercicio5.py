2cdddb90d82f749443bef32975a531f840383575
Agirre8 <91331083+Agirre8@users.noreply.github.com>
2022-02-18 20:42:52 +0100

    Add files via upload

---
+++ ejercicio5.py
@@ -0,0 +1,16 @@
+def import os
+
+
+niños
+def entrada
+if niños == 2
+    entrada = entrada*0.9
+if niños == 3
+    entrada = entrada*0.85
+if niños == 4
+    entrada = entrada*0.82
+if niños >= 5
+niños = cantidadniños
+descuento = cantidadniños - 4
+descuentofinal = (82 - descuento)/100
+entrada = entrada*descuentofinal
