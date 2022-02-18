b05ffff57f3c6c3fe38ca7b9661895c403052c7e
Agirre8 <91331083+Agirre8@users.noreply.github.com>
2022-02-18 21:11:21 +0100

    Add files via upload

---
+++ Ejercicio6.py
@@ -0,0 +1,29 @@
+def from wsgiref.simple_server import demo_app
+from xmlrpc.client import boolean
+
+
+COMMAQ: boolean
+BEL: boolean
+descuento: int
+componentes: int
+
+if 10000>componentes>20000
+    descuento = 0.1
+    if COMMAQ True
+    descuento =  descuento + 0.01
+    if BEL = true 
+    descuento = descuento 0.02
+
+if 20001>componentes>30000
+    descuento = 0.15
+    if COMMAQ True
+    descuento =  descuento + 0.01
+    if BEL = true 
+    descuento = descuento 0.02
+
+if componentes>40000
+    descuento = 0.2
+    if COMMAQ True
+    descuento =  descuento + 0.01
+    if BEL = true 
+    descuento = descuento 0.02
